5275b0d1fd270224946367aa3d3c1e5073eb8020
Seiyial Aurum <sayhao@seiyial.xyz>
2021-10-11 11:30:53 +0800

    Change timers

--- src/main/java/me/skarless/Main.java
+++ src/main/java/me/skarless/Main.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 public class Main {
 
     // Bot Settings
-    public static String TOKEN = "YOUR TOKEN HERE";
-    public static boolean DEBUG = true;
-    public static int DELAY_MIN = 10;
-    public static int DELAY_MAX = 20;
+    public static final String TOKEN = "YOUR TOKEN HERE";
+    public static final boolean DEBUG = true;
+    public static final int DELAY_MIN = 10;
+    public static final int DELAY_MAX = 35;
 
     // In the beginning, there was public static void main(String[] args)
     public static void main(String[] args) throws IOException {
